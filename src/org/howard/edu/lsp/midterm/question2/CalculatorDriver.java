fc7b305e1db0ea63d49827b11924165073e66a0e
dagrif1223 <dagrif1223@gmail.com>
2024-10-17 20:16:33 -0400

    added missing code

--- src/org/howard/edu/lsp/midterm/question2/CalculatorDriver.java
+++ src/org/howard/edu/lsp/midterm/question2/CalculatorDriver.java
@@ -1,2 +1,17 @@
-package org.howard.edu.lsp.midterm.question2;public class CalculatorDriver {
+package org.howard.edu.lsp.midterm.question2;
+
+/**
+ * A driver class to demonstrate the functionality of the Calculator utility class.
+ */
+public class CalculatorDriver {
+    public static void main(String[] args) {
+        // Demonstrate sum of two integers
+        System.out.println(Calculator.sum(5, 10));            // Output: 15 (sum of two integers)
+
+        // Demonstrate sum of two doubles
+        System.out.println(Calculator.sum(3.5, 7.8));        // Output: 11.3 (sum of two doubles)
+
+        // Demonstrate sum of an array of integers
+        System.out.println(Calculator.sum(new int[]{1, 2, 3, 4, 5})); // Output: 15 (sum of array of integers)
+    }
 }
